4a8489f978ee02a9f7cf8c53d1f613a4d9a3f9a3
counter185 <33550839+counter185@users.noreply.github.com>
2022-02-26 23:03:59 +0100

    there it is, after all those years. a working script.

---
+++ AETEXtoTGA.ps1
@@ -0,0 +1,19 @@
+﻿param(
+    [Parameter(Mandatory=$true)][string]$fileName
+)
+
+while ($fileName.StartsWith("`"")){
+    $fileName = $fileName.Substring(1)
+}
+
+while ($fileName.EndsWith("`"")){
+    $fileName = $fileName.Substring(0, $fileName.Length-1)
+}
+
+$fileBytes = [System.IO.File]::ReadAllBytes($fileName)
+
+
+$outName = $fileName + ".tga"
+
+
+[System.IO.File]::WriteAllBytes($outName, $fileBytes[0x38..($fileBytes.length)])
