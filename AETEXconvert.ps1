011042a8120ce7a2217d99095e4603125129fd1c
counter185 <33550839+counter185@users.noreply.github.com>
2023-02-03 17:59:42 +0100

    Detect PS Vita GXT files

--- AETEXconvert.ps1
+++ AETEXconvert.ps1
@@ -13,6 +13,7 @@ while ($fileName.EndsWith("`"")){
 $fileBytes = [System.IO.File]::ReadAllBytes($fileName)
 
 $isDDS = $fileBytes[0x34] -eq 0x44 -and $fileBytes[0x35] -eq 0x44 -and $fileBytes[0x36] -eq 0x53
+$isGXT = $fileBytes[0x38] -eq 0x47 -and $fileBytes[0x39] -eq 0x58 -and $fileBytes[0x3A] -eq 0x54
 
 $outName = $fileName
 $startIndex = 0x38
@@ -20,6 +21,8 @@ $startIndex = 0x38
 if ($isDDS){
     $outName += ".dds"
     $startIndex = 0x34
+} elseif ($isGXT) {
+	$outName += ".gxt"
 } else {
     $outName += ".tga"
 }
